af5c1b660c5636b58ceca293d1e6349607e3e3f6
Juan Vivanco <juansebastianvc@gmail.com>
2020-11-11 00:16:43 -0500

    Spell fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const Index = () => {
   return (
     <Layout>
       <Metadata title="Home" description="This is my home page" />
-      <h2 className={indexStyles.title}>Hello I'm Juan. A Front End Developer based in Ecuador. Father of a Beautifull 👸🏼 and 🪂 Fanatic.</h2>
+      <h2 className={indexStyles.title}>Hello I'm Juan. A Front End Developer based in Ecuador. Father of a Beautiful 👸🏼 and 🪂 Fanatic.</h2>
       <h3 className={indexStyles.description}>I specialise in creating interactive experiences and functional interfaces using Javascript, React, Vue, SASS and Design Systems.</h3>
       <h2 className={indexStyles.titleClients}>Last Clients</h2>
       <h3 className={indexStyles.description}>TeamQ, ISA GmbH, Laderach, WebCreek, Nabors, The Woodlands Financial Group, Lloyd's Register</h3>
