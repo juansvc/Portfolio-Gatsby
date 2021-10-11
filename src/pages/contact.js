4253a0990536b7c8c9eb40481b7c7b1db7889eb1
Juan Vivanco <juan.vivanco@alation.com>
2021-10-11 11:47:59 -0500

    Update CV

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import Metadata from "../components/metadata"
 import indexStyles from "./index.module.scss"
-import {FaInstagram, FaGithub, FaLinkedin} from "react-icons/fa"
+import {FaInstagram, FaGithub, FaLinkedin, FaRegAddressBook} from "react-icons/fa"
 
 
 
@@ -12,7 +12,8 @@ const Contact = () => {
       <Metadata title="Contact" description="Get in touch with us" />
       <h1 className={indexStyles.title}>You can find me on<br/><br/> <FaInstagram /> via <a href="https://www.instagram.com/juansvivanco/" target="_blank">@juansvivanco</a>
       <br/> <FaGithub /> via <a href="https://github.com/juansvc" target="_blank">@juansvc</a>
-      <br/> <FaLinkedin /> via <a href="https://www.linkedin.com/in/juansvivanco/" target="_blank">@juansvivanco</a></h1>
+      <br/> <FaLinkedin /> via <a href="https://www.linkedin.com/in/juansvivanco/" target="_blank">@juansvivanco</a>
+      <br/> <FaRegAddressBook /> via <a href="https://bit.ly/2YHwLKN" target="_blank">CV PDF</a></h1>
     </Layout>
   )
 }
