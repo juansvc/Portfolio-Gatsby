651a2c7ca0a09b8dbc627a8531db8c71419e7931
Juan Vivanco <juansebastianvc@gmail.com>
2022-08-25 14:57:08 -0500

    UpdateCV

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,7 +13,7 @@ const Contact = () => {
       <h1 className={indexStyles.title}>You can find me on<br/><br/> <FaInstagram /> via <a href="https://www.instagram.com/juansvivanco/" target="_blank">@juansvivanco</a>
       <br/> <FaGithub /> via <a href="https://github.com/juansvc" target="_blank" rel="noreferrer">@juansvc</a>
       <br/> <FaLinkedin /> via <a href="https://www.linkedin.com/in/juansvivanco/" target="_blank" rel="noreferrer">@juansvivanco</a>
-      <br/> <FaRegAddressBook /> via <a href="https://www.icloud.com/iclouddrive/07TAQFKa_M_JPCmpDzTHwH0Sg#Juan_Vivanco_CV" target="_blank" rel="noreferrer">CV PDF</a></h1>
+      <br/> <FaRegAddressBook /> via <a href="https://www.icloud.com/iclouddrive/044uGi6XWzAOiakf3OcEhOFkw#CV_Engineer_Juan_Vivanco" target="_blank" rel="noreferrer">CV PDF</a></h1>
     </Layout>
   )
 }
